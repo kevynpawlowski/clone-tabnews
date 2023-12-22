0cd12aad20cdba16ad6772090bba9e1d9db54675
Kevyn Pawlowski <154021165+kevynpawlowski@users.noreply.github.com>
2023-12-22 21:35:01 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se vim no meu quarto em 10 segundos ganha 5 reais :)</h1>
+  return <h1>Se vim no meu quarto em 10 segundos ganha 5 reais :)</h1>;
 }
 
 export default Home;
